ce01a254b0c10e8db2b304ad5d5fcab498fe1548
Mikhail Snitavets <mksn13@gmail.com>
2017-03-27 11:41:44 +0300

    Revert "Add @SpringBootApplication to make IDEA understand, that Application is a main Spring Boot class"
    This reverts commit f17eaf8745b9b611fbd6833f031762e623cc049d.

--- src/main/java/by/triumgroup/recourse/Application.java
+++ src/main/java/by/triumgroup/recourse/Application.java
@@ -2,11 +2,11 @@ package by.triumgroup.recourse;
 
 import by.triumgroup.recourse.configuration.MainConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(MainConfiguration.class, args);
     }
+
 }
