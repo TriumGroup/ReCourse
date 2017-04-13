89bb31ac737ffc2e1223a777db490322cb82f6a0
vanashimko <vana.shimko@gmail.com>
2017-04-13 23:38:22 +0300

    Add hometask solution controller tests

--- src/test/java/by/triumgroup/recourse/controller/impl/HometaskSolutionControllerTest.java
+++ src/test/java/by/triumgroup/recourse/controller/impl/HometaskSolutionControllerTest.java
@@ -9,19 +9,44 @@ import by.triumgroup.recourse.service.HometaskSolutionService;
 import by.triumgroup.recourse.service.MarkService;
 import by.triumgroup.recourse.supplier.entity.model.EntitySupplier;
 import by.triumgroup.recourse.supplier.entity.model.impl.HometaskSolutionSupplier;
+import by.triumgroup.recourse.supplier.entity.model.impl.MarkSupplier;
+import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.util.Optional;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 public class HometaskSolutionControllerTest extends CrudControllerTest<HometaskSolution, Integer> {
+    private static final String MARK_REQUEST = "/hometask/solution/1/mark";
     private HometaskSolutionController hometaskSolutionController;
     private HometaskSolutionService hometaskSolutionService;
     private HometaskSolutionSupplier hometaskSolutionSupplier;
     private MarkService markService;
+    private MarkSupplier markSupplier;
 
     public HometaskSolutionControllerTest() {
         markService = Mockito.mock(MarkService.class);
         hometaskSolutionService = Mockito.mock(HometaskSolutionService.class);
         hometaskSolutionController = new HometaskSolutionControllerImpl(hometaskSolutionService, markService);
         hometaskSolutionSupplier = new HometaskSolutionSupplier();
+        markSupplier = new MarkSupplier();
+    }
+
+    @Test
+    public void getMarkTest() throws Exception {
+        when(markService.findBySolutionId(any())).thenReturn(Optional.of(markSupplier.getValidEntityWithId()));
+        sendGet(MARK_REQUEST)
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getSolutionsNotExistingHometaskTest() throws Exception {
+        when(markService.findBySolutionId(any())).thenReturn(Optional.empty());
+        sendGet(MARK_REQUEST)
+                .andExpect(status().isNotFound());
     }
 
     @Override
