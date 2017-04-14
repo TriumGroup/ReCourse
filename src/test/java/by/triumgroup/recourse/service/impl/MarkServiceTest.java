a4fc1f2fb87d9e9c272b373c6562865aa08f3eac
vanashimko <vana.shimko@gmail.com>
2017-04-14 09:28:19 +0300

    Add mark service tests

--- src/test/java/by/triumgroup/recourse/service/impl/MarkServiceTest.java
+++ src/test/java/by/triumgroup/recourse/service/impl/MarkServiceTest.java
@@ -7,8 +7,17 @@ import by.triumgroup.recourse.service.CrudServiceTest;
 import by.triumgroup.recourse.service.MarkService;
 import by.triumgroup.recourse.supplier.entity.model.EntitySupplier;
 import by.triumgroup.recourse.supplier.entity.model.impl.MarkSupplier;
+import org.junit.Assert;
+import org.junit.Test;
 import org.mockito.Mockito;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class MarkServiceTest extends CrudServiceTest<Mark, Integer> {
     private MarkService markService;
@@ -20,6 +29,27 @@ public class MarkServiceTest extends CrudServiceTest<Mark, Integer> {
         markService = new MarkServiceImpl(markRepository);
         markSupplier = new MarkSupplier();
     }
+
+    @Test
+    public void findByExistingSolutionIdTest() throws Exception {
+        when(markRepository.findBySolutionId(any())).thenReturn(markSupplier.getValidEntityWithId());
+
+        Optional<Mark> result = markService.findBySolutionId(markSupplier.getAnyId());
+
+        verify(markRepository, times(1)).findBySolutionId(any());
+        Assert.assertTrue(result.isPresent());
+    }
+
+    @Test
+    public void findByNotExistingSolutionIdTest() throws Exception {
+        when(markRepository.findBySolutionId(any())).thenReturn(null);
+
+        Optional<Mark> result = markService.findBySolutionId(markSupplier.getAnyId());
+
+        verify(markRepository, times(1)).findBySolutionId(any());
+        Assert.assertFalse(result.isPresent());
+    }
+
 
     @Override
     protected CrudService<Mark, Integer> getCrudService() {
